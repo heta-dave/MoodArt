96f9127d12605bb5eac6e88cf1e217636b2c6785
Heta Dave <142527328+heta-dave@users.noreply.github.com>
2025-07-22 20:41:50 +0530

    Ensuring correct port binding for Render

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -15,4 +16,5 @@ def predict():
     return jsonify({"mood": prediction})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
